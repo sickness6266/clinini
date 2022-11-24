c351a16b4e4111bdb3b3b5eb3788217055f805ac
sickness6266 <116468600+sickness6266@users.noreply.github.com>
2022-11-24 18:05:21 -0300

    Delete clinicaapp directory

--- clinicaapp/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from clinicaapp.models import Cita
-class Citafiltro(admin.ModelAdmin):
-    list_display=("id", "nombre")
-    search_fields=("id", "nombre") 
-admin.site.register(Cita, Citafiltro)    
-# Register your models here.
