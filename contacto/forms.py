e2a8baf5ec3e7c8b15cf361cf90b872d155951a4
sickness6266 <116468600+sickness6266@users.noreply.github.com>
2022-11-24 18:05:30 -0300

    Delete contacto directory

--- contacto/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-
-class formulariocontacto(forms.Form):
-    nombre=forms.CharField(label="Nombre",required=True)
-    email=forms.CharField(label="Email",required=True)
-    contenido=forms.CharField(label="Contenido", widget=forms.Textarea)
-    
