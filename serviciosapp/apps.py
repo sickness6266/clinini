d8b38694c0ec821531892efc3089789dede1506d
sickness6266 <116468600+sickness6266@users.noreply.github.com>
2022-11-24 18:06:27 -0300

    Delete serviciosapp directory

--- serviciosapp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ServiciosappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'serviciosapp'
--- serviciosapp/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from serviciosapp.models import Servicio
-
-# Create your views here.
-def servicios(request):
-    servicios=Servicio.objects.all()
-    return render(request,"servicios/servicios.html",{"servicios":servicios})
